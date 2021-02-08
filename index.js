04248d333477a740d9cf3d6562b4043369589001
Vivek <vivekmunde@gmail.com>
2021-02-08 14:56:44 +0530

    development mode source map

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-import arrayToMapStateToProps from './src/array-to-map-state-to-props';
-import connectDispatch from './src/connect-dispatch';
-import connectState from './src/connect-state';
-import connect from './src/connect';
-import createStore from './src/create-store';
-import injectDispatch from './src/inject-dispatch';
-import Provider from './src/provider';
+'use strict';
 
-export {
-    arrayToMapStateToProps,
-    connectDispatch,
-    connectState,
-    connect,
-    createStore,
-    injectDispatch,
-    Provider
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./dist/duxact.production.js');
+} else {
+  module.exports = require('./dist/duxact.development.js');
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import arrayToMapStateToProps from './array-to-map-state-to-props';
+import connectDispatch from './connect-dispatch';
+import connectState from './connect-state';
+import connect from './connect';
+import createStore from './create-store';
+import injectDispatch from './inject-dispatch';
+import Provider from './provider';
+
+export {
+  arrayToMapStateToProps,
+  connectDispatch,
+  connectState,
+  connect,
+  createStore,
+  injectDispatch,
+  Provider
+}
