9a319d17a7aee30bee5f4b5956da3547a5efc6c4
Vivek <vivekmunde@gmail.com>
2021-02-08 14:56:44 +0530

    development mode source map

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import arrayToMapStateToProps from './array-to-map-state-to-props';
+import connectDispatch from './connect-dispatch';
+import connectState from './connect-state';
+import connect from './connect';
+import createStore from './create-store';
+import injectDispatch from './inject-dispatch';
+import Provider from './provider';
+import useDispatch from './use-dispatch';
+import useSelector from './use-selector';
+
+export {
+  arrayToMapStateToProps,
+  connectDispatch,
+  connectState,
+  connect,
+  createStore,
+  injectDispatch,
+  Provider,
+  useDispatch,
+  useSelector,
+};
